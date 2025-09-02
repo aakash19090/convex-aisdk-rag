c46343e05d5dcadba0ff23694024178d20c26778
Akash Sharma <aakashsharma19090@gmail.com>
2025-09-03 00:17:20 +0530

    add dialog box on click of note

--- src/app/(main)/notes/note-item.tsx
+++ src/app/(main)/notes/note-item.tsx
@@ -2,15 +2,24 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Doc } from '../../../../convex/_generated/dataModel';
+import { NotePreviewDialog } from './note-preview-dialog';
 
 export interface NoteItemProps {
     note: Doc<'notes'>;
 }
 
 export function NoteItem({ note }: NoteItemProps) {
+    const handleOpenNote = () => {
+        // using window.history.pushState without useRouter as its faster and to change the URL without reloading
+        window.history.pushState({}, '', `?noteId=${note._id}`);
+    };
+
     return (
         <>
-            <Card className='cursor-pointer hover:shadow-md transition-shadow'>
+            <Card
+                className='cursor-pointer hover:shadow-md transition-shadow'
+                onClick={handleOpenNote}
+            >
                 <CardHeader>
                     <CardTitle> {note.title} </CardTitle>
                 </CardHeader>
@@ -20,6 +29,7 @@ export function NoteItem({ note }: NoteItemProps) {
                     </div>
                 </CardContent>
             </Card>
+            <NotePreviewDialog note={note} />
         </>
     );
 }
--- src/app/(main)/notes/note-preview-dialog.tsx
+++ src/app/(main)/notes/note-preview-dialog.tsx
@@ -1,30 +1,43 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import { Trash2 } from "lucide-react";
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
+import { Trash2 } from 'lucide-react';
+import { Doc } from '../../../../convex/_generated/dataModel';
+import { useSearchParams } from 'next/navigation';
 
-export function NotePreviewDialog() {
-  return (
-    <Dialog>
-      <DialogContent className="sm:max-w-[625px]">
-        <DialogHeader>
-          <DialogTitle>Note Title</DialogTitle>
-        </DialogHeader>
-        <div className="mt-4 whitespace-pre-wrap">Note Body</div>
-        <DialogFooter className="mt-6">
-          <Button variant="destructive" className="gap-2">
-            <Trash2 size={16} />
-            Delete Note
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+export interface NotePreviewDialogProps {
+    note: Doc<'notes'>;
+}
+
+export function NotePreviewDialog({ note }: NotePreviewDialogProps) {
+    const searchParams = useSearchParams();
+    const isOpen = searchParams.get('noteId') === note._id;
+
+    const handleClose = () => {
+        window.history.pushState({}, '', '/notes');
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={handleClose}>
+            <DialogContent className='sm:max-w-[625px]'>
+                <DialogHeader>
+                    <DialogTitle> {note.title} </DialogTitle>
+                </DialogHeader>
+                <div className='mt-4 whitespace-pre-wrap'>{note.body}</div>
+                <DialogFooter className='mt-6'>
+                    <Button variant='destructive' className='gap-2'>
+                        <Trash2 size={16} />
+                        Delete Note
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
 }
